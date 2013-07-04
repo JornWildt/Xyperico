0ba61c8690b784196777775671914bed93d68673
JornWildt <jw@fjeldgruppen.dk>
2013-07-04 07:43:09 +0200

    Adding JSON serializer for document store.

---
+++ Xyperico.Agres/Xyperico.Agres.JsonNet/JsonNetStreamSerializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Xyperico.Agres.DocumentStore;
+
+
+namespace Xyperico.Agres.JsonNet
+{
+  public class JsonNetStreamSerializer : IStreamSerializer
+  {
+    JsonSerializer Serializer = new JsonSerializer();// { TypeNameHandling = TypeNameHandling.Auto };
+
+
+    public void Serialize(Stream s, object item)
+    {
+      using (var tw = new StreamWriter(s, Encoding.UTF8))
+      using (var jw = new JsonTextWriter(tw))
+      {
+        Serializer.Serialize(jw, item);
+      }
+    }
+
+
+    public object Deserialize(Type t, Stream s)
+    {
+      using (var tr = new StreamReader(s, Encoding.UTF8))
+      using (var jr = new JsonTextReader(tr))
+      {
+        return Serializer.Deserialize(jr, t);
+      }
+    }
+  }
+}
---
+++ Xyperico.Agres/Xyperico.Agres.Tests/DocumentStore/JsonNetFileDocumentStoreTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Xyperico.Agres.DocumentStore;
+using Xyperico.Agres.JsonNet;
+
+
+namespace Xyperico.Agres.Tests.DocumentStore
+{
+  [TestFixture]
+  public class JsonNetFileDocumentStoreTests : AbstractDocumentStoreTests
+  {
+    protected override IDocumentStore<string, long> BuildDocumentStore_Int()
+    {
+      JsonNetStreamSerializer serializer = new JsonNetStreamSerializer();
+      return new FileDocumentStore<string, long>(StorageBaseDir, serializer);
+    }
+
+
+    protected override IDocumentStore<string, MySerializableData> BuildDocumentStore_Class()
+    {
+      JsonNetStreamSerializer serializer = new JsonNetStreamSerializer();
+      return new FileDocumentStore<string, MySerializableData>(StorageBaseDir, serializer);
+    }
+  }
+}
