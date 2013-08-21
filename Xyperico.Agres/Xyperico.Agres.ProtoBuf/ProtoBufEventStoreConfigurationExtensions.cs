979dec5719b7630a1b1f4c9c5fbbe922514efba1
JornWildt <jw@fjeldgruppen.dk>
2013-08-21 22:07:10 +0200

    Adding the last serializer.

--- Xyperico.Agres/Xyperico.Agres.ProtoBuf/ProtoBufEventStoreConfigurationExtensions.cs
+++ Xyperico.Agres/Xyperico.Agres.ProtoBuf/ProtoBufEventStoreConfigurationExtensions.cs
@@ -1,11 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Xyperico.Agres.EventStore;
+using Xyperico.Agres.Serialization;
+using Xyperico.Agres.DocumentStore;
+
 
 namespace Xyperico.Agres.ProtoBuf
 {
   public static class ProtoBufEventStoreConfigurationExtensions
   {
+    /// <summary>
+    /// Use ProtoBuf for event serialization.
+    /// </summary>
+    /// <param name="cfg"></param>
+    /// <returns></returns>
+    public static EventStoreConfiguration WithProtoBufEventSerializer(this EventStoreConfiguration cfg)
+    {
+      ISerializer serializer = new ProtoBufSerializer();
+      Xyperico.Agres.EventStore.EventStoreConfigurationExtensions.SetMessageSerializer(cfg, serializer);
+      return cfg;
+    }
+
+
+    /// <summary>
+    /// Use ProtoBuf for document serialization.
+    /// </summary>
+    /// <param name="cfg"></param>
+    /// <returns></returns>
+    public static EventStoreConfiguration WithProtoBufDocumentSerializer(this EventStoreConfiguration cfg)
+    {
+      IDocumentSerializer serializer = new ProtoBufDocumentSerializer();
+      Xyperico.Agres.EventStore.EventStoreConfigurationExtensions.SetDocumentSerializer(cfg, serializer);
+      return cfg;
+    }
   }
 }
